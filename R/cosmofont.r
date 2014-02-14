728f6a375a53c3cd4dff83c0855620a2b356df9e
seankross <smk240@gmail.com>
2014-02-14 17:24:53 -0500

    implementing google_font_options

--- R/cosmofont.r
+++ R/cosmofont.r
@@ -16,7 +16,55 @@
 #' cosmofont("~/Desktop/My_Fonts", weight=500, style="italic")
 #' cosmofont()
 #' }
-cosmofont <- (..., font_dir=getwd(), weight=400, style="normal", export_path=getwd()){
+cosmofont <- (..., uri=F){
   # Capture settings
   settings <- list(...)
+}
+
+#'
+
+cosmofont_easy <- function(font_dir=getwd(), weight=400, style="normal", export_path=getwd(), uri=F){
+  
+}
+
+#'
+font_options  <- function(){
+  
+}
+
+#'
+#' @importFrom stringr str_extract perl
+#' @importFrom httr GET content
+google_font_options <- function(font_name, bold=F, italic=F, weight=400){
+  # Make sure weight is in the realm of sanity
+  if(weight < 100 || weight > 1000){
+    stop("weight must be between 100 and 1000")
+  }
+  
+  # Build query for GET request
+  font_query <- paste0("family=", space2plus(font_name), ":")
+  if(italic){
+    font_query <- paste0(font_query, "i")
+  }
+  if(weight != 400){
+    font_query <- paste0(font_query, weight)
+  } else if(bold){
+    font_query <- paste0(font_query, "b")
+  }
+  
+  # Make GET request for CSS
+  response <- GET("http://fonts.googleapis.com/", path = "css", query = font_query)
+  
+  # Get file url
+  url <- sub("url\\(", "", str_extract(content(response, "text"), perl("url([^)]+)")))
+  
+  # Make GET request for font file
+  font_file <- GET(url)
+  
+  
+  list(name = font_name, weight = weight, style = style, format = 'tff', file = font_file, option = "google")
+}
+
+space2plus <- function(word){
+  sub(" ", "+", word, fixed=T)
 }
