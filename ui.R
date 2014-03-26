4d159f22bad5bcc49bc0269b9c612e62c55842f2
seankross <smk240@gmail.com>
2014-03-26 19:39:57 -0400

    added shiny app

---
+++ ui.R
@@ -0,0 +1,25 @@
+library(shiny)
+
+# Define UI for application that plots random distributions 
+shinyUI(bootstrapPage(
+
+  # Use custom CSS
+  includeCSS("stylesheet.css"),
+
+  # Application title
+  headerPanel("Hello Shiny!"),
+
+  # Sidebar with a slider input for number of observations
+  sidebarPanel(
+    sliderInput("obs", 
+                "Number of observations:", 
+                min = 1,
+                max = 1000, 
+                value = 500)
+  ),
+
+  # Show a plot of the generated distribution
+  mainPanel(
+    plotOutput("distPlot")
+  )
+))
